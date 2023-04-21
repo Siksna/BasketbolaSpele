9221e9c4ca49c9e18e125928d1b8cd637de42f6e
Siksna <ulfsssiksnaGmail.com>
2023-04-21 14:05:13 +0300

    Izveidots iesakums, nestrada dazas lietas bet ir izveidots un butu jastrada 2 ainam, teksta ievadei un izvadei, un ir pievienotas izmantojamas bildes

---
+++ BasketbolaSpeletaji/Assets/Skripti/Ainas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ainas : MonoBehaviour {
+
+	// Use this for initialization
+	public void UzSakumu(){
+		SceneManager.LoadScene ("Ievads", LoadSceneMode.Single);
+	}
+
+	public void UzSpeli(){
+		SceneManager.LoadScene (1, LoadSceneMode.Single);
+	}
+
+}
---
+++ BasketbolaSpeletaji/Assets/Skripti/Teksti.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teksti : MonoBehaviour {
+	
+	private string iesauka;
+	private int vecums;
+
+	public GameObject ievadesLauks1;
+	public GameObject ievadesLauks2;
+
+	public GameObject tekstaAttelosana;
+
+
+	public void UzglabatTekstu(){
+
+		do{
+			vecums=int.Parse(ievadesLauks2.GetComponent<Text>().text);
+		}while(vecums<18 || vecums>45);
+		iesauka = ievadesLauks1.GetComponent<Text> ().text;
+		tekstaAttelosana.GetComponent<Text>().text = 
+			iesauka+" ir "+vecums+" gadus vecs";
+	}
+}
+
