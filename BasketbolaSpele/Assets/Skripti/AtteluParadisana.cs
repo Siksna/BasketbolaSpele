ac20436f2d5382afc40ff37e54ce7a71b4d1848a
Siksna <ulfsssiksna@gmail.com>
2023-04-24 16:12:57 +0300

    Izveidots dropdown kur var mainit speletajus

---
+++ BasketbolaSpele/Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour {
+	
+	public GameObject mainigaisAttels;
+	public Sprite[] atteluMasivs;
+	public GameObject IzmeraSlaideris;
+
+
+
+
+	public void Izkritosais(int indekss)
+	{
+		if(indekss == 0)
+			mainigaisAttels.GetComponent<Image>().sprite 
+			= atteluMasivs[0];
+
+		else if (indekss == 1)
+			mainigaisAttels.GetComponent<Image>().sprite
+			= atteluMasivs[1];
+
+		else if (indekss == 2)
+			mainigaisAttels.GetComponent<Image>().sprite
+			= atteluMasivs[2];
+	}
+
+	public void mainitLielumu()
+	{
+		float pasreizejaisIzmers =
+			IzmeraSlaideris.GetComponent<Slider>().value;
+		mainigaisAttels.transform.localScale =
+			new Vector2(1f* pasreizejaisIzmers,
+				1f*pasreizejaisIzmers);
+	}
+}
